7b680df6d3767efe95164fa0f97a78c39b215803
saiteja-in <vurukondasaiteja13@gmail.com>
2024-09-28 15:10:28 +0530

    final commit

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const http = require('http');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ;
 
 http.createServer(function(request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World!');
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end(`
+    <html>
+      <head>
+        <style>
+          body {
+            background-color: #f2f2f2;
+            font-family: Arial, sans-serif;
+            text-align: center;
+          }
+          h1 {
+            color: #007bff;
+            margin-top: 50px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Hello Azure!!</h1>
+      </body>
+    </html>
+  `);
 }).listen(port);
 
 console.log(`Server running at http://localhost:${port}`);
